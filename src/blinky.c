b5eab80e4f15721b5eb6da8b577baa1fc136ead2
cmbuck <cmbuck@vt.edu>
2014-09-30 16:04:32 -0500

    fixed bits to green LED

--- src/blinky.c
+++ src/blinky.c
@@ -8,14 +8,14 @@ GPIO_InitTypeDef GPIO_InitStructure;
 int main(void)
 {
        
-  /* GPIOD Periph clock enable */
+  /* GPIOB Periph clock enable */
   //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
-  /* Configure PD0 and PD2 in output pushpull mode */
+  /* Configure PB5 in output pushpull mode */
 //  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
 //  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 //  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-//  GPIO_Init(GPIOD, &GPIO_InitStructure);
+//  GPIO_Init(GPIOB, &GPIO_InitStructure);
 
   /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory. 
      You can monitor PD0 or PD2 on the scope to measure the output signal. 
@@ -24,54 +24,54 @@ int main(void)
      This code needs to be compiled with high speed optimization option.  */
   while (1)
   {
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
 
-    /* Set PD0 and PD2 */
-    GPIOD->BSRR = 0x00000005;
-    /* Reset PD0 and PD2 */
-    GPIOD->BRR  = 0x00000005;
+    /* Set PB5 */
+    GPIOB->BSRR = 0x00000010;
+    /* Reset PB5 */
+    GPIOB->BRR  = 0x00000010;
   }
 }

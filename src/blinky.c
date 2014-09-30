aa1df701f4da25c48308eeaa810e25d81b88a53c
cmbuck <cmbuck@vt.edu>
2014-09-30 15:16:28 -0500

    Fixed blinky.c: includes, libs, undefined macros.  Now builds

--- src/blinky.c
+++ src/blinky.c
@@ -1,6 +1,7 @@
+#define STM32F10X_MD
+
 #include "stm32f10x.h"
 #include "stm32f10x_conf.h"
-#include "stm32_eval.h"
 
 GPIO_InitTypeDef GPIO_InitStructure;
 
@@ -8,13 +9,13 @@ int main(void)
 {
        
   /* GPIOD Periph clock enable */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+  //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
   /* Configure PD0 and PD2 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
+//  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+//  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+//  GPIO_Init(GPIOD, &GPIO_InitStructure);
 
   /* To achieve GPIO toggling maximum frequency, the following  sequence is mandatory. 
      You can monitor PD0 or PD2 on the scope to measure the output signal. 

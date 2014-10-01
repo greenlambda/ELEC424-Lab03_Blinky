8e5354fbeed1fa414d44e143da5b8e7242d0e71e
cmbuck <cmbuck@vt.edu>
2014-09-30 21:04:08 -0500

    Now blinks via timed loops

--- src/blinky.c
+++ src/blinky.c
@@ -5,18 +5,21 @@ GPIO_InitTypeDef GPIO_InitStructure;
 
 int main(void) {
 	/* GPIOD Periph clock enable */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
 	/* Configure PD0 and PD2 in output pushpull mode */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_2;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	volatile int i = 0;
 
 	while (1) {
-		/* Set PD0 and PD2 */
-		GPIOD->BSRR = 0x00000005;
-		/* Reset PD0 and PD2 */
-		GPIOD->BRR = 0x00000005;
+		/* Set PB5 */
+		GPIOB->BSRR = GPIO_Pin_5;
+		for (i = 0; i < 10000000; i++);
+		/* Reset PB5 */
+		GPIOB->BRR = GPIO_Pin_5;
+		for (i = 0; i < 10000000; i++);
 	}
 }

6d06adfb5fc713ba6234f4684e5c23769b2f8ccb
cmbuck <cmbuck@vt.edu>
2014-09-30 22:30:53 -0500

    blinky now works using timers and interrupts

--- src/blinky.c
+++ src/blinky.c
@@ -9,6 +9,7 @@ TIM_OCInitTypeDef  TIM_OCInitStructure;
 TIM_BDTRInitTypeDef TIM_BDTRInitStructure;
 uint16_t TimerPeriod = 0;
 uint16_t Channel1Pulse = 0, Channel2Pulse = 0, Channel3Pulse = 0;
+volatile unsigned char stateLED = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 void RCC_Configuration(void);
@@ -35,25 +36,50 @@ void InitializeTimer()
     TIM_TimeBaseInitTypeDef timerInitStructure;
     timerInitStructure.TIM_Prescaler = 40000;
     timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    timerInitStructure.TIM_Period = 800;
+    timerInitStructure.TIM_Period = 200;
     timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
     timerInitStructure.TIM_RepetitionCounter = 0;
     TIM_TimeBaseInit(TIM2, &timerInitStructure);
     TIM_Cmd(TIM2, ENABLE);
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+}
+
+void EnableTimerInterrupt()
+{
+    NVIC_InitTypeDef nvicStructure;
+    nvicStructure.NVIC_IRQChannel = TIM2_IRQn;
+    nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    nvicStructure.NVIC_IRQChannelSubPriority = 1;
+    nvicStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&nvicStructure);
 }
 
 int main()
 {
     InitializeLEDs();
     InitializeTimer();
+    EnableTimerInterrupt();
 
     for (;;)
     {
-        int timerValue = TIM_GetCounter(TIM2);
-        if (timerValue == 400)
-            GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET);
-        else if (timerValue == 700)
-            GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
+//        int timerValue = TIM_GetCounter(TIM2);
+//        if (timerValue == 400)
+//            GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET);
+//        else if (timerValue == 700)
+//            GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
+    }
+}
+
+void TIM2_IRQHandler()
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    {
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+        if (stateLED)
+        	GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET);
+        else
+        	GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET);
+        stateLED = 1 - stateLED;
     }
 }
 

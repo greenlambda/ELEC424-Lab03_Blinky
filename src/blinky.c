df9633bc3763dbb1c561a906052ad26932d575ba
cmbuck <cmbuck@vt.edu>
2014-10-13 10:44:47 -0500

    Changed from TIM2 to TIM4

--- src/blinky.c
+++ src/blinky.c
@@ -18,12 +18,12 @@ void InitializePWMChannel()
 {
     TIM_OCInitTypeDef outputChannelInit;
     outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
-    outputChannelInit.TIM_Pulse = TIM2PERIOD / 8 * 7;
+    outputChannelInit.TIM_Pulse = TIM4PERIOD / 8;
     outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
     outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
 
-    TIM_OC1Init(TIM2, &outputChannelInit);
-    TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+    TIM_OC1Init(TIM4, &outputChannelInit);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
 
     //GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
 }
@@ -47,26 +47,26 @@ static void InitializeLEDs() {
  * Initialize the timer and set the clock to the external oscillator
  */
 static void InitializeTimer() {
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 	RCC_SYSCLKConfig(RCC_SYSCLKSource_HSE); /* set clock to HSE */
 
 	TIM_TimeBaseInitTypeDef timerInitStructure;
-	timerInitStructure.TIM_Prescaler = TIM2PRESCALER;
+	timerInitStructure.TIM_Prescaler = TIM4PRESCALER;
 	timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	timerInitStructure.TIM_Period = TIM2PERIOD;
+	timerInitStructure.TIM_Period = TIM4PERIOD;
 	timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	timerInitStructure.TIM_RepetitionCounter = 0;
-	TIM_TimeBaseInit(TIM2, &timerInitStructure);
-	TIM_Cmd(TIM2, ENABLE);
-	TIM_ITConfig(TIM2, TIM_IT_Update | TIM_IT_CC1, ENABLE);
+	TIM_TimeBaseInit(TIM4, &timerInitStructure);
+	TIM_Cmd(TIM4, ENABLE);
+	TIM_ITConfig(TIM4, TIM_IT_Update | TIM_IT_CC1, ENABLE);
 }
 
 /*
- * Enable the interrupt for the timer (TIM2)
+ * Enable the interrupt for the timer (TIM4)
  */
 static void EnableTimerInterrupt() {
 	NVIC_InitTypeDef nvicStructure;
-	nvicStructure.NVIC_IRQChannel = TIM2_IRQn;
+	nvicStructure.NVIC_IRQChannel = TIM4_IRQn;
 	nvicStructure.NVIC_IRQChannelPreemptionPriority = 0;
 	nvicStructure.NVIC_IRQChannelSubPriority = 1;
 	nvicStructure.NVIC_IRQChannelCmd = ENABLE;
@@ -83,36 +83,41 @@ int main() {
 	InitializePWMChannel();
 
 	/* Loop. Forever. */
-	for (;;) {
+	int i;
+	for (i = 0;i < 1000000; i++) {
+		//wait
 	}
+	TIM_SetCompare1(TIM4, TIM4PERIOD / 2);
+	for (;;);
+
 }
 
 /*
  * Interrupt service routine.  This function handles toggling the LED
  * when the timer throws an update flag.
  */
-void TIM2_IRQHandler() {
+void TIM4_IRQHandler() {
 	ITStatus updateSig,compareSig;
-	updateSig = TIM_GetITStatus(TIM2, TIM_IT_Update);
-	compareSig = TIM_GetITStatus(TIM2, TIM_IT_CC1);
-//	int timerValue1 = TIM_GetCounter(TIM2);
+	updateSig = TIM_GetITStatus(TIM4, TIM_IT_Update);
+	compareSig = TIM_GetITStatus(TIM4, TIM_IT_CC1);
+//	int timerValue1 = TIM_GetCounter(TIM4);
 	static unsigned char stateLED = 0;
-//	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET &&
-//		TIM_GetITStatus(TIM2, TIM_IT_CC1) != RESET) {
+//	if (TIM_GetITStatus(TIM4, TIM_IT_Update) != RESET &&
+//		TIM_GetITStatus(TIM4, TIM_IT_CC1) != RESET) {
 //		GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET); /* Turn off LED */
 //	}
 	/* Overflow event */
 	if (updateSig != RESET) {
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update); /* clear flag */
+		TIM_ClearITPendingBit(TIM4, TIM_IT_Update); /* clear flag */
 		GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_RESET); /* Turn on LED */
 		//stateLED = 1 - stateLED; /* flip the state for next operation */
 	}
 	/* Capture Control event */
 	else if (compareSig != RESET) {
-		TIM_ClearITPendingBit(TIM2, TIM_IT_CC1); /* clear flag */
+		TIM_ClearITPendingBit(TIM4, TIM_IT_CC1); /* clear flag */
 		GPIO_WriteBit(GPIOB, GPIO_Pin_5, Bit_SET); /* Turn off LED */
 		//stateLED = 1 - stateLED; /* flip the state for next operation */
 	}
-//	int timerValue2 = TIM_GetCounter(TIM2);
+//	int timerValue2 = TIM_GetCounter(TIM4);
 
 }
